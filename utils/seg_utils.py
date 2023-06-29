6558dec6a6fc7c88a33c496d02b7ad37127fb95c
Vision <gitesh.ch.0912@gmail.com>
2023-06-29 17:37:37 +0530

    Support for uploading images and masks

--- utils/seg_utils.py
+++ utils/seg_utils.py
@@ -9,6 +9,7 @@ from PIL import Image
 
 from configs.seg_config import LAYERS_CONFIG
 
+
 def set_control_args(control_args: typing.Dict, layer_args: typing.Dict):
     """Use `st.select_slider` or `st.slider` for `control_args` depending on
     default value.
@@ -19,7 +20,9 @@ def set_control_args(control_args: typing.Dict, layer_args: typing.Dict):
             if isinstance(layer_args[key], str):
                 options = value
                 default_value = layer_args[key]
-                new_value = st.selectbox(key, options=options, index=options.index(default_value))  # Fixed the argument duplication
+                new_value = st.selectbox(
+                    key, options=options, index=options.index(default_value)
+                )  # Fixed the argument duplication
             else:
                 min_value = value[0]
                 max_value = value[1]
@@ -52,15 +55,13 @@ def select_layer_seg_aug():
 
     return layer
 
+
 def download_images_seg():
     root_dir = "images/seg/"
     default_images = {}
 
     image_data = {
-        "city": {
-            "image_name": "image_1.png",
-            "mask_name": "mask_1.png"
-        },
+        "city": {"image_name": "image_1.png", "mask_name": "mask_1.png"},
     }
 
     for name, image_info in image_data.items():
@@ -80,19 +81,30 @@ def download_images_seg():
     return default_images
 
 
-def image_dropdown(image_dict=download_images_seg()):
-    st.subheader("Select an Image")
+def image_dropdown(image_dict=None):
+    if image_dict is None:
+        image_dict = download_images_seg()
+
+    st.subheader("Upload Image and Mask")
+    uploaded_image = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
+    uploaded_mask = st.file_uploader("Upload Mask", type=["jpg", "jpeg", "png"])
+
+    if uploaded_image and uploaded_mask:
+        image = Image.open(uploaded_image).convert("RGB")
+        mask = Image.open(uploaded_mask).convert("RGB")
+        return np.array(image), np.array(mask)
+
+    st.subheader("Or select an existing image")
     image_option = st.selectbox(
         "Select an option",
         list(image_dict.keys()),
         index=0,
         key="image_option",
     )
-    image_data = image_dict[image_option]
 
+    image_data = image_dict[image_option]
     image = Image.open(io.BytesIO(image_data["image"])).convert("RGB")
     mask = Image.open(io.BytesIO(image_data["mask"])).convert("RGB")
-
     return np.array(image), np.array(mask)
 
 
@@ -110,7 +122,7 @@ def Preprocessing(layer, image, mask):
     mask = tf.convert_to_tensor(mask, dtype=tf.float32)
     mask = tf.expand_dims(mask, axis=0)
 
-    inputs = {"images": image, "segmentation_masks":mask}
+    inputs = {"images": image, "segmentation_masks": mask}
 
     outputs = layer(inputs)
     return inputs, outputs
@@ -118,17 +130,32 @@ def Preprocessing(layer, image, mask):
 
 def display_img_with_mask(inputs, outputs):
     col1, col2 = st.columns(2)
-    
+
     with col1:
         st.subheader("Input Image and Mask")
-        st.image(np.array(inputs['images']).astype(np.uint8), use_column_width=True, caption='Image')
-        st.image(np.array(inputs['segmentation_masks']).astype(np.uint8), use_column_width=True, caption='Augmented Mask')
-            
+        st.image(
+            np.array(inputs["images"]).astype(np.uint8),
+            use_column_width=True,
+            caption="Image",
+        )
+        st.image(
+            np.array(inputs["segmentation_masks"]).astype(np.uint8),
+            use_column_width=True,
+            caption="Mask",
+        )
+
     with col2:
         st.subheader("Output Image and Mask")
-        st.image(np.array(outputs["images"]).astype(np.uint8), use_column_width=True, caption='Augmented Image')
-        st.image(np.array(outputs["segmentation_masks"]).astype(np.uint8), use_column_width=True, caption='Augmented Mask')
-
+        st.image(
+            np.array(outputs["images"]).astype(np.uint8),
+            use_column_width=True,
+            caption="Augmented Image",
+        )
+        st.image(
+            np.array(outputs["segmentation_masks"]).astype(np.uint8),
+            use_column_width=True,
+            caption="Augmented Mask",
+        )
 
 
 def seg():
